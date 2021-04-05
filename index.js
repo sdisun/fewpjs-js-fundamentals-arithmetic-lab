8cd06b1e2d71c7c1412099eb3244d62dfaa43f57
Neil <zooming.in@hotmail.com>
2021-04-05 20:36:07 +1000

    updates

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
 
-// Write your code here
+let random;
+random = Math.floor(Math.random()*10);
+
+let mod;
+let num3 = 14;
+let num4 = 5;
+mod = num3 % num4;
+
+let max;
+let num5 = 20;
+let num6 = 19;
+max = Math.max(num5, num6);
